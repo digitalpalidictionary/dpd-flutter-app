e88fae7e5c77a3eb38a44a5c4ea01555761e19ba
bdhrs <bodhirasa@gmail.com>
2026-04-01 07:20:00 +0530

    chore: cleanup root dir

--- test_query.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-void main() {
-  String rootsStr = '["√har 1", "√har 2", "√har 3", "√har 4"]';
-  final decoded = jsonDecode(rootsStr) as List;
-  List<String> keys = [];
-  for (final key in decoded) {
-    if (key is String && key.isNotEmpty) keys.add(key);
-  }
-}
